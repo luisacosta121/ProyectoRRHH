cbb2633a88f860cc59380762e7a605ed032cc63e
luisacosta121 <luisacosta121@gmail.com>
2024-11-29 17:09:06 -0300

    Se generan calculos para recibos de sueldo

--- ProyectoRRHH/Models/ReciboSueldo.cs
+++ ProyectoRRHH/Models/ReciboSueldo.cs
@@ -15,10 +15,19 @@ namespace ProyectoRRHH.Models
 
 
         public double SueldoNeto => SueldoBruto * (1 - Constantes.Descuento);
+        public double Jubilacion => SueldoBruto * (Constantes.DescuentoJubilacion);
+        public double ObraSocial => SueldoBruto * (Constantes.DescuentoObraSocial);
+        public double Ley19032 => SueldoBruto * (Constantes.DescuentoLey);
     }
 
     public static class Constantes
     {
-        public const double Descuento = 0.17; // 17% de descuento
+        public const double Descuento = 0.17; // Descuento total
+
+        public const double DescuentoJubilacion = 0.11; // 11% descuento jubilacion
+
+        public const double DescuentoObraSocial = 0.03; // 3% descuento obra social
+
+        public const double DescuentoLey = 0.03; // 3% descuento ley 19032
     }
 }
